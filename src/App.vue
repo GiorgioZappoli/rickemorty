eeebb49ccab1afc6da960eb707f1a11e1cdcf3cd
giorgio <giorgio.zappoli@studio.unibo.it>
2024-10-24 13:11:07 +0200

    deleted favicon and helloworld.vue, modified index.html and app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderSection></HeaderSection>
+
+    <div class="lower-section">
+      <FourDivs></FourDivs>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HeaderSection from './HeaderSection.vue';
+  import FourDivs from './FourDivs.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default{
+    components:{
+      HeaderSection,
+      FourDivs
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

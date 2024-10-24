f802aa72810998e13435f02ff63b8adbec83dbfd
giorgio <giorgio.zappoli@studio.unibo.it>
2024-10-24 13:07:57 +0200

    added HeaderSection, FourDivs, Card.json

---
+++ src/FourDivs.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="four-divs">
+        <div class="div-item" v-for="n in 4" :key="n">
+                <img :src="Clipboard.image" alt="immagine" />
+                <p>{{ Clipboard.name }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import cardsData from '../public/card.json';
+
+    export default{
+        data(){
+            return{
+                cards: cardsData
+            };
+        }
+    }
+</script>
+
+<style scoped>
+    .four-divs{
+        display: flex;
+        justify-content: space-around;
+    }
+
+    div.item{
+        width: 200;
+        text-align: center;
+        margin: 10px;
+        background-color: grey;
+    }
+
+    .div-item img{
+        width: 50%;
+        height: auto;
+    }
+</style>

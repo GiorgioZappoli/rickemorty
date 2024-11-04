32e9cdac53ce18612fb0209e62f19275892b030b
giorgio <giorgio.zappoli@studio.unibo.it>
2024-11-04 17:50:55 +0100

    feat: :sparkles: initialized backend

---
+++ rickemorty/src/main/java/com/marcello/rickemorty/RickemortyApplication.java
@@ -0,0 +1,13 @@
+package com.marcello.rickemorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickemortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickemortyApplication.class, args);
+	}
+
+}
---
+++ rickemorty/src/test/java/com/marcello/rickemorty/RickemortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcello.rickemorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickemortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

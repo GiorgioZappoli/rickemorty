954cce49ebb449cc8f84b253a5dc3a0106de148e
giorgio <giorgio.zappoli@studio.unibo.it>
2024-11-06 17:46:38 +0100

    feat: :art: fixed code style

--- rickemorty/src/main/java/com/marcello/rickemorty/JsonController.java
+++ rickemorty/src/main/java/com/marcello/rickemorty/JsonController.java
@@ -1,15 +1,11 @@
 package com.marcello.rickemorty;
 
-
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
-
-
-
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -21,12 +17,10 @@ public class JsonController {
         return "Hello World";
     }
     
-    
     @GetMapping("/data")
     public ResponseEntity<String> getJsonData() throws IOException{
         ClassPathResource jsonResource = new ClassPathResource("resources.json");
         String jsonData = new String(jsonResource.getInputStream().readAllBytes());
         return ResponseEntity.ok(jsonData);
-    }
-    
+    }    
 }

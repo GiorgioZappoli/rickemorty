bfafc59c5127049a2dabe5e31d4a869d1103b3e3
giorgio <giorgio.zappoli@studio.unibo.it>
2024-11-08 17:36:33 +0100

    feat: :hammer: finished script to populate the db

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,43 @@
-const fs = require("fs");
-const { MongoClient } = require("mongodb");
+import { readdirSync, statSync, readFileSync } from "fs";
+import { MongoClient } from "mongodb";
+import { join } from "path";
 
-const filePath = "..\\ignored\\characters\\page1.json";
+const filePath = "..\\ignored\\characters";
 const uri = "mongodb://localhost:8090";
 const dbName = "RickAndMorty";
 const collectionName = "Characters";
+
+const folderPath = filePath;
+
+const files = readdirSync(folderPath);
+
+insertData();
 
 async function insertData() {
   const client = new MongoClient(uri);
 
   try {
     await client.connect();
-    console.log("ok");
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
-    const data = fs.readFileSync(filePath, "utf8");
-    const jsonData = JSON.parse(data);
+    for (let i = 0; i < files.length; i++) {
+      const filePath = join(folderPath, files[i]);
+      const data = readFileSync(filePath, "utf8");
 
-    const characters = jsonData.results;
-    console.log(characters);
+      const jsonData = JSON.parse(data);
+      const characters = jsonData.results;
+      console.log(characters);
 
-    if (Array.isArray(characters)) {
-      await collection.insertMany(characters);
-      console.log("ok2");
-    } else {
-      console.log("cazzo");
-      await collection.insertOne(characters);
+      if (Array.isArray(characters)) {
+        await collection.insertMany(characters);
+      } else {
+        console.log("error");
+      }
     }
   } catch (error) {
-    console.error("Errore", error);
+    console.error("Error", error);
   } finally {
     await client.close();
   }
 }
-
-insertData();
